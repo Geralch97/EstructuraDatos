83a395bb49fbb173e518a6153685d5194d4969b0
Gerardo Chacón <geralch@hotmail.es>
2017-08-28 08:53:25 -0500

    Practica 2 - Estrcutura de Datos

---
+++ src/Practica2/SNode.java
@@ -0,0 +1,18 @@
+package Practica2;
+
+public class SNode<T> {
+	public T value;
+	public SNode<T> next;
+	
+	public SNode()
+	{
+		value = null;
+		next = null;
+	}
+	
+	public SNode(T value)
+	{
+		this.value = value;
+		next = null;
+	}
+}
