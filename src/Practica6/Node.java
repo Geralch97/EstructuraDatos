c68baf059fd64ed7d5e2861640ea3f5c259d3a61
Gerardo Chacón <geralch@hotmail.es>
2017-09-19 10:57:40 -0500

    Practica 6 - Queues - Gerardo López Chacón

---
+++ src/Practica6/Node.java
@@ -0,0 +1,21 @@
+//Gerardo López Chacón | A01379707
+
+package Practica6;
+
+public class Node<T> {
+	public T value;
+	public Node<T> prev;
+	public Node<T> next;
+	
+	public Node() {
+		value = null;
+		prev = this;
+		next = this;
+	}
+	
+	public Node(T value) {
+		this.value = value;
+		prev = this;
+		next = this;
+	}
+}
