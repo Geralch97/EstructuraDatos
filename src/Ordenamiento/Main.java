3e7f2d204e7b8ef7fa240d2a2807a508903dd57d
Gerardo Chacón <geralch@hotmail.es>
2017-10-09 17:50:37 -0500

    Ordenamiento

--- src/Ordenamiento/Main.java
+++ src/Ordenamiento/Main.java
@@ -16,6 +16,8 @@ public class Main {
         int[] Insertion = {2, 15, 7, 1, 10};
         int[] Selection = {4, 9, 5, 0, -2};
         int[] Bubble = {6, 17, 10, 1, 0};
+        int[] Heap = {1, 0, 24, 18, -2, 10};
+        
                 
         
         insertionsort(Insertion);
@@ -24,6 +26,10 @@ public class Main {
         System.out.println(Arrays.toString(Selection));
         bubblesort(Bubble);
         System.out.println(Arrays.toString(Bubble));
+        makeheap(Heap);
+        System.out.println(Arrays.toString(Heap));
+        System.out.println(removeTopItem(Heap, Heap.length));
+        System.out.println(Arrays.toString(Heap));
         
     }
     
@@ -76,5 +82,62 @@ public class Main {
                 
             }
         }
+    }
+    
+    public static void makeheap (int values[]){
+        for(int i = 0; i < values.length; i++){
+            int index = i;
+            while (index != 0){
+                int parent = (index - 1) / 2;
+                
+                if (values[index] <= values[parent]){
+                    break;
+                }
+                
+                int temp = values[index];
+                values [index] = values[parent];
+                values [parent] = temp;
+                
+                index = parent;
+            }
+        }
+        
+    }
+    
+    public static int removeTopItem (int values[], int count){
+        int result = values[0];
+        values[0] = values [count - 1]; // ultimo elemento de nuestra lista
+        
+        int index = 0;
+        while (true){
+            int child1 = 2 * index + 1;
+            int child2 = 2 * index + 2;
+            
+            if(child1 >= count){
+                child1 = index;
+            }
+            
+            if(child2 >= count){
+                child2 = index;
+            }
+            
+            if(values[index] >= values[child1] && values[index] >= values[child2]){
+                break;
+            }
+            int swapChild = child1;
+            if(values[child2] > values[child1]){
+                swapChild = child2;
+            }
+            
+            int temp = values[index];
+            values[index] = values[swapChild];
+            values[swapChild] = temp;
+            
+            index = swapChild;
+        }
+        return result;
+        
+        
+        
     }
 }
